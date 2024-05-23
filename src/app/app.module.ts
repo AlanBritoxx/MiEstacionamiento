5bed5c9caa44203c2282fee24b829b4505ae5f5e
yoeljoaq <joaq.carcamo@duocuc.cl>
2024-05-23 12:49:58 -0400

    firebase main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,19 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { IonicStorageModule } from '@ionic/storage-angular'; // Importa Ionic Storage
 import { ModalOfrecerComponent } from './modal-ofrecer/modal-ofrecer.component';
 import { ModalNuevoModal } from './modal-nuevo/modal-nuevo.modal';
+import {AngularFireModule} from '@angular/fire/compat';
+import {AngularFireAuthModule} from '@angular/fire/compat/auth'
+
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   declarations: [AppComponent, ModalInformacionComponent, ModalOfrecerComponent, ModalNuevoModal],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot(),
+    AngularFireModule, AngularFireAuthModule,AngularFireModule.initializeApp(environment.firebaseConfig)],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,66 +1,131 @@
 import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
-import { Component, ViewChild, OnInit} from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ModalController } from '@ionic/angular';
-
+import { LoadingController, ModalController, AlertController } from '@ionic/angular';
 import { HttpClient } from '@angular/common/http';
 import { Storage } from '@ionic/storage-angular';
-
+import { AuthenticationService } from '../authentication.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
-
+export class HomePage implements OnInit {
+  regForm: FormGroup;
+  showPassword = false;
+  passwordToggleIcon = 'eye';
   slideAnimationActive: boolean = false;
-  NombreForm: FormGroup;
+
   usuario: string = '';
   password: string = '';
   CContrasena: string = '';
   telefono: number = 0;
 
-  constructor(private modalController: ModalController, private formBuilder: FormBuilder, private router: Router, private route: ActivatedRoute, private http: HttpClient, private storage: Storage) {
-    this.NombreForm = this.formBuilder.group({
-      nombre: ['', [Validators.required, Validators.minLength(3)]],
-      apellido: ['', [Validators.required, Validators.minLength(4)]],
-      
-    });
-    
-  }
+  constructor(
+    private modalController: ModalController,
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private http: HttpClient,
+    private storage: Storage,
+    public loadingCtrl: LoadingController,
+    public authService: AuthenticationService,
+    public router: Router,
+    private alertCtrl: AlertController
+  ) {}
+
   initializeDatabase() {
     this.storage.create();
   }
-  
 
   goToLogin() {
     this.router.navigate(['/inicio']);
   }
 
   ngOnInit() {
-    this.initializeDatabase();
+    this.regForm = this.formBuilder.group({
+      fullname: ['', [Validators.required]],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email,
+          Validators.pattern("[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$"),
+        ],
+      ],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern("^(?=.*[A-Z0-9])(?=.*[0-9])(?=.*[A-Z]).{8,}$"),
+        ],
+      ],
+    });
   }
 
+  get errorControl() {
+    return this.regForm?.controls;
+  }
 
-  
+  async signUp() {
+    const loading = await this.loadingCtrl.create();
+    await loading.present();
+
+    if (this.regForm?.valid) {
+      const user = await this.authService
+        .registerUser(this.regForm.value.email, this.regForm.value.password)
+        .catch(async (error) => {
+          console.log(error);
+          await loading.dismiss();
+          this.showAlert('Registration Error', error.message);
+        });
+
+      if (user) {
+        await loading.dismiss();
+        this.router.navigate(['/inicio']);
+      }
+    } else {
+      await loading.dismiss();
+      this.showAlert('Formulario invalido', 'Completa los campos correctamente');
+    }
+  }
+
+  async showAlert(header: string, message: string) {
+    const alert = await this.alertCtrl.create({
+      header,
+      message,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
+  togglePassword(): void {
+    this.showPassword = !this.showPassword;
+
+    if (this.passwordToggleIcon == 'eye') {
+      this.passwordToggleIcon = 'eye-off';
+    } else {
+      this.passwordToggleIcon = 'eye';
+    }
+  }
 
   register() {
-
-  
     const userData = {
       usuario: this.usuario,
       password: this.password,
-
     };
     console.log('Datos a guardar:', userData);
-  
+
     this.storage.set('userData', userData).then(() => {
       console.log('Datos guardados en Ionic Storage');
       this.router.navigate(['/test']);
     });
   }
-  
+}
+
+
+function errorControl() {
+  throw new Error('Function not implemented.');
 }
